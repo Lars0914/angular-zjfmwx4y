68985818bd44cbc2f79b690e332a1340d8780800
Lars0914 <dev.remote0914@gmail.com>
2026-03-14 18:30:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zjfmwx4y
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Lars0914/angular-zjfmwx4y)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import {
+  KENDO_SPREADSHEET,
+  SheetDescriptor,
+} from "@progress/kendo-angular-spreadsheet";
+import { sheets } from "./sheets";
+
+@Component({
+  selector: "my-app",
+  imports: [KENDO_SPREADSHEET],
+  template: `
+    <kendo-spreadsheet [sheets]="sheets" style="height: 600px; width: 100%">
+    </kendo-spreadsheet>
+  `,
+})
+export class AppComponent {
+  public sheets: SheetDescriptor[] = sheets;
+}
